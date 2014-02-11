f87c084b8f626da477a8673da08c7e75e3dc9b9c
Sandon <the_sandon@hotmail.com>
2014-02-11 20:10:32 +0800

    on 140211

--- LTranslation.js
+++ LTranslation.js
@@ -1,1 +1,6 @@
-
+//create a configure context item for 'page' context type
+function optionsOnClick(info, tab){
+    window.location.href="www.google.com";
+}
+var id = chrome.contextMenus.create({"title": "≈‰÷√'L∑≠“Î'", "contexts":["page"],
+    "onclick": genericOnClick});
